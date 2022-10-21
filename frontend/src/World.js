7cc7ffbccdfc1c41aff7c56877d8fef8cbb56192
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2022-10-21 21:06:57 +0200

    feat: add globe

---
+++ frontend/src/World.js
@@ -0,0 +1,12 @@
+import Globe from "react-globe.gl";
+
+const World = () => {
+  return (
+    <Globe
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+      backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+    ></Globe>
+  );
+};
+
+export default World;

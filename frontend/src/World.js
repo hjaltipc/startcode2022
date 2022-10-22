4c285a046c20597b88ec8774db488a22663da562
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2022-10-22 12:30:43 +0200

    feat: add match data

--- frontend/src/World.js
+++ frontend/src/World.js
@@ -1,10 +1,69 @@
+import { useEffect, useState } from "react";
 import Globe from "react-globe.gl";
+import data from "./matchlist-21-05-2022.txt";
+
+import map from "./stock-vector-world-map-vector-1299977560.jpg";
 
 const World = () => {
+  let url =
+    "https://dev.fn.sportradar.com/common/en/Europe:Berlin/gismo/match_coordinates/";
+
+  const urlFeed =
+    "https://dev.fn.sportradar.com/common/en/Europe:Oslo/gismo/event_get";
+
+  const [matches, setMatches] = useState([]);
+  const [points, setPoints] = useState([]);
+
+  const fetchdata = (match) => {
+    fetch(url + match, { "Access-Control-Allow-Origin": "*" })
+      .then((res) => res.json())
+      .then((rows) => {
+        if (rows.doc[0].data.coordinates != null) {
+          let split = rows.doc[0].data.coordinates.split(",");
+          // console.log(split[0]);
+          let newDot = {
+            lat: parseInt(split[0]),
+            lng: parseInt(split[1]),
+            size: Math.random() / 3,
+            color: "red",
+          };
+
+          setPoints((points) => [...points, newDot]);
+        }
+      });
+  };
+
+  useEffect(() => {
+    fetch(urlFeed)
+      .then((r) => r.json())
+      .then((text) => {
+        setMatches(text["doc"][0].data);
+      });
+  }, []);
+
+  const removePoint = () => {
+    setTimeout(() => {
+      const newPointsArray = points.slice(1, points.length);
+      setPoints(newPointsArray);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    if (matches.length > 0) {
+      for (const match of matches) {
+        let id = match.match._id;
+        fetchdata(id);
+        removePoint();
+      }
+    }
+  }, [matches]);
+
   return (
     <Globe
-      globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-      backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+      globeImageUrl={"//unpkg.com/three-globe/example/img/earth-day.jpg"}
+      pointsData={points}
+      pointAltitude="size"
+      pointColor="color"
     ></Globe>
   );
 };

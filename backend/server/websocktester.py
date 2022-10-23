e439186786d242218c029aa55d6dab9cd7f6b9e9
martin0x48 <penguin@laptop>
2022-10-23 11:59:00 +0200

    add some more

--- backend/server/websocktester.py
+++ backend/server/websocktester.py
@@ -1,8 +1,10 @@
 import asyncio
 import websockets
 import ssl
+from loguru import logger
 
 async def test():
+    logger.info("Starting websocket func")
     async with websockets.connect('wss://127.0.0.1:6921',ssl=ssl.CERT_NONE) as websocket:
         await websocket.send("hello")
         response = await websocket.recv()

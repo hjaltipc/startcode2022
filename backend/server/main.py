bb018e8c4e608cbc372718bc55192ae0835ab4b2
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2022-10-23 14:16:23 +0200

    Feature: displaying data

--- backend/server/main.py
+++ backend/server/main.py
@@ -2,59 +2,98 @@ from fastapi import Request, FastAPI
 import queue
 import uvicorn
 import threading
-from re import M
-from fastapi import Request, FastAPI
-import queue
-from geodata import get_coordinate
-import uvicorn
-import threading
+import random
+import websockets
+import asyncio
+from loguru import logger
+import time
 import json
+from geodata import get_coordinate
 
 dataQueue = queue.Queue()
+websocketSendQueue = queue.Queue()
+# socketQueue = queue.Queue() #Que for the info that the socker is going to send to the react websocket
+
 
 app = FastAPI()
+
+
 @app.get("/")
 async def root():
-    return {"message":"Hello World"}
+    return {"message": "The API WORKS! WHOOOHOOO"}
 
-#json
+
+async def handler(websocket, path):
+    WebQueueObj = websocketSendQueue
+    logger.info("Starting websocket handler")
+    data = await websocket.recv()
+    reply = f"Data recieved as: {data}"
+    print(f"Data recieved from client  is {data}")
+    await websocket.send(reply)
+    while True:
+        if WebQueueObj.not_empty:
+            data = WebQueueObj.get()
+            await websocket.send(str(data))
+            print("Not empty")
+        else:
+            time.sleep(0.1)
+            print("empty")
+            # Prevent loop checking all the time
+
+        #dat = random.randint(0,500)
+        # await websocket.send(str(dat))
+        # time.sleep(1)
+
 
 @app.post("/input")
 async def get_body(request: Request):
-    #rq = await request.json()
     rq = await request.body()
-    #print(rq)
+    # print(f"recieved {rq} from server")
     dataQueue.put(rq)
-    return "success"
 
 
+def websocketThreadFunction(eventLoop, webQueueObj=websocketSendQueue):
+    logger.info("Starting websocketThread")
 
-def printer(queueob):
-    while(True): 
+    start_server = websockets.serve(handler, "127.0.0.1", 6921, loop=eventLoop)
+    eventLoop.run_until_complete(start_server)
+    eventLoop.run_forever()
+    logger.info("WebsocketThread stopping")
+
+
+def printer(queueob, webQueueObj):
+    while (True):
         try:
             if queueob.not_empty:
-                queueObj = queueob.get().decode() #.decode()[8:]
+                queueObj = queueob.get().decode()  # .decode()[8:]
                 try:
                     matchId = json.loads(queueObj)["matchId"]
                 except:
-                    matchId = queueObj.split("=")[1].replace("!@#$%^&*()[]{};:,./<>?\|`~-=_+", " ")
+                    matchId = queueObj.split("=")[1].replace(
+                        "!@#$%^&*()[]{};:,./<>?\|`~-=_+", " ")
                 coordinate = get_coordinate(matchId)
                 print(coordinate)
+                if coordinate["lat"] != None:
+                    webQueueObj.put(coordinate)
 
         except Exception as e:
             print(e)
 
 
-
-    
-
-
-
-
 if __name__ == "__main__":
-    printerThread = threading.Thread(target=printer, args=(dataQueue,))
+    printerThread = threading.Thread(
+        target=printer, args=(dataQueue, websocketSendQueue,))
     printerThread.start()
 
+    eventLoop = asyncio.new_event_loop()
+    websocketThread = threading.Thread(
+        target=websocketThreadFunction, args=(eventLoop, websocketSendQueue,))
+    websocketThread.start()
 
+    try:
+        uvicorn.run(app, host="0.0.0.0", port=6969)
+    except KeyboardInterrupt:
+        logger.info("Exiting because KeyboardInterrupt")
+        exit()
 
-    uvicorn.run(app,host="0.0.0.0", port=6969)
+    logger.info("Program exiting")
--- frontend/src/World.js
+++ frontend/src/World.js
@@ -1,32 +1,103 @@
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
+import { useCallback, useEffect } from "react";
 import Globe from "react-globe.gl";
-
+import { useSocket } from "./useSocket";
 
 const World = () => {
-  const ws = new WebSocket('ws://localhost:6921')
-  ws.addEventListener('open', (event) => {
-    ws.send('Present');
-  });
+  const globeEl = useRef();
+  const [points, setPoints] = useState([]);
+  const socket = useSocket();
 
-  ws.addEventListener('message', (event) => {
-      if(event){
-        console.log("connection established")
-        console.log(event);
+  const setPointsFrom = (lat, long) => {
+    let newDot = {
+      lat: lat,
+      lng: long,
+      maxR: 2,
+      propagationSpeed: (Math.random() - 0.5) * 10 + 1,
+      repeatPeriod: Math.random() * 200 + 200,
+    };
+    setPoints((points) => [...points, newDot]);
+    removePoint();
+    console.log(newDot);
+  };
+
+  const onMessage = useCallback((event) => {
+    console.log(event.data);
+
+    if (event.data) {
+      if (event.data != "Data recieved as: Present") {
+        // const lat = JSON.parse(event.data);
+        const data1 = event.data.replace("}", "").replace("{", "");
+        const data2 = data1.split(",");
+        const data3 = data2[0].split(":");
+        console.log(data3[1]);
+        const data4 = data2[1].split(":");
+        console.log(data4[1]);
+        console.log(data4);
+
+        // console.log(lat);
+        // console.log(lat.type);
+        // const long = event.data.long;
+        setPointsFrom(parseInt(data3[1]), parseInt(data4[1]));
+        // console.log("connection established");
+        // console.log(event.data.toString());
+        // console.log(event.type);
+        // // console.log(parseInt(newDot));
+
+        // console.log("Point set");
       }
-      console.log("MORE")
-      let msg = (event.data);
-      //let msgContent = msg.lat +","+msg.long;
-      console.log(event.data.toString());
-    
-   });
+
+      // const parsedDot = parseInt(newDot);
+    }
+  }, []);
+
+  const removePoint = () => {
+    setTimeout(() => {
+      console.log(points);
+      const newPointsArray = points.slice(0, 1);
+      setPoints(newPointsArray);
+      console.log("Point removed");
+    }, 8700);
+  };
+
+  useEffect(() => {
+    socket.addEventListener("open", (event) => {
+      console.log("sent");
+      socket.send("Present");
+    });
+
+    console.log("listener made");
+    socket.addEventListener("message", onMessage);
+
+    // globeEl.current.controls().autoRotate = true;
+    // globeEl.current.controls().autoRotateSpeed = 2;
+
+    // setTimeout(() => {
+    //   const directionalLight = globeEl.current
+    //     .scene()
+    //     .children.find((obj3d) => obj3d.type === "DirectionalLight");
+    //   directionalLight && directionalLight.position.set(0, 0, 0); // change light position to see the specularMap's effect
+    // });
+
+    // return socket.removeEventListener("message", onMessage);
+  }, [socket, onMessage]);
+
+  const colorInterpolator = (t) => `rgba(220,20,60,${Math.sqrt(1 - t)})`;
 
   return (
     <div className="globe">
       <Globe
-        globeImageUrl="https://assets.vercel.com/image/upload/v1595320886/front/home/globe-texture.jpg"
-        backgroundColor="rgba(0,0,0,0)"
-        width={1000}
-        height={500}
+        ref={globeEl}
+        showAtmosphere={false}
+        globeImageUrl={
+          "https://assets.vercel.com/image/upload/v1595320886/front/home/globe-texture.jpg"
+        }
+        backgroundColor="rgba(0, 0, 0, 0)"
+        ringsData={points}
+        ringColor={() => colorInterpolator}
+        ringMaxRadius="maxR"
+        ringPropagationSpeed="propagationSpeed"
+        ringRepeatPeriod="repeatPeriod"
       ></Globe>
     </div>
   );
